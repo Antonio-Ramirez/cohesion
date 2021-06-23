0ec0d30e98851ca2cb006c1d706dff44a0f81518
AntonioRamirez <aramirez@momentfeed.com>
2021-06-23 17:56:14 -0300

    minor changes

--- src/main/scala/com/cohesion/challenge/ApplicationServer.scala
+++ src/main/scala/com/cohesion/challenge/ApplicationServer.scala
@@ -1,11 +1,8 @@
 package com.cohesion.challenge
 
-import com.cohesion.challenge.subscriber.ServiceContext
-
 object ApplicationServer extends ServiceContext {
 
   def main(args: Array[String]): Unit = {
     logger.info(s"Starting service $nameApplication")
   }
-
 }
